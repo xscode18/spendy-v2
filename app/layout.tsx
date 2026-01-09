6aba7d339f1f2e630efd6f912420358857ab3f95
xscode18 <62397500+xscode18@users.noreply.github.com>
2026-01-09 09:20:57 -0700

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: "Sendy",
+  description: "Sendy party board game"
+};
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0 }}>
+        {children}
+      </body>
+    </html>
+  );
+}
